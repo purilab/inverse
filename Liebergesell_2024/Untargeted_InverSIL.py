e7ed5e74a1de01acda9833f1eb7d4d98bf484570
purilab <81434442+purilab@users.noreply.github.com>
2024-08-28 13:23:20 -0600

    Update Untargeted_InverSIL.py
    -Added cell that calculates unique 12C and 13C features from MZmine alignment,
    -Updated neutron steps to display values for all steps from 12C to 13C, not just those with signal in the precursor condition
    -Added lines that converted strings to 'float' for numerical values in columns
    -Added a way for the script to automatically loop through all samples in the loaded_dict dictionary

--- Liebergesell_2024/Untargeted_InverSIL.py
+++ Liebergesell_2024/Untargeted_InverSIL.py
@@ -18,18 +18,31 @@ import numpy as np
 import os
 import copy
 
-os.chdir("insert the path to the metatlas package here")  #change file path as needed
+os.chdir("/Users/tashiliebergesell/Desktop/Puri Lab/Coding/metatlas-3.0.0")  #change file path as needed
 from metatlas.io import feature_tools as ft
+
 import math
 from scipy.signal import argrelextrema
 import glob
+
+# progress bar
 from tqdm import tqdm
 
 #%%files list
-os.chdir("import the path to the folder containing the XXX_1213.csv file here") #Change path as needed
-samples = glob.glob(os.path.join('XXX_1213.csv'))  #Rename as needed. Import the data from the .csv file. 
+os.chdir("/Users/tashiliebergesell/Desktop/20240814_AWP273_diff trace/0uM Cu, 5uM Fe") #Change path as needed
+samples = glob.glob(os.path.join('AWP273_1213.csv'))  #Rename as needed. 
 samples = pd.DataFrame(samples)
 samples[0] = samples[0].apply(lambda x: os.path.basename(x).split('_')[0])
+
+#%%calculate unique 12C and 13C features found in each condition
+
+df = pd.read_csv("./AWP273_1213.csv")    
+df = df.dropna(axis = 1)
+df.columns = pd.Series(['RT','MZ_12C','13'])
+df12 = df[df['MZ_12C'] == (df['MZ_12C'] + df['13'])] #unique 12C features
+df13 = df[df['13'] == (df['MZ_12C'] + df['13'])] #unique 13C features
+print(str(len(df12)) + " unique 12C features")
+print(str(len(df13)) + " unique 13C features")
 
 #%%combos
 combos = {}
@@ -42,8 +55,8 @@ for n in range(len(samples)):
     df.drop(align_12C_13C, inplace = True) 
     tw = df[df["MZ_12C"].notnull()].loc[:,["RT","MZ_12C"]]
     th = df[df["13"].notnull()].loc[:,["RT","13"]]
-    mz_tolerance = 1    # Define m/z tolerance [m/z]. Change if needed
-    rt_tolerance = 0.1  # Define RT tolerance [min]. Change if needed
+    mz_tolerance = 5    # Define m/z tolerance [m/z]. Change if needed
+    rt_tolerance = 0.1  # Define RT tolerance [min]. 
     tw["_temp"] = 0
     th["_temp"] = 0
     combined = th.merge(tw, 'outer', '_temp', suffixes=['_13C', '_12C'])
@@ -64,7 +77,6 @@ for n in range(len(samples)):
             if combined.iat[i,0] != 0:
                 if combined.iat[i,3]-(combined.iat[i,3]/200000) < combined.iat[j,3] < combined.iat[i,3]+(combined.iat[i,3]/200000):
                     if abs(combined.iat[i,2] - combined.iat[j,2]) < .05:
-                        
                             combined.iat[i,0] = 0
     combined = combined.replace(0, np.nan)
     combined = combined.dropna(axis = 0)
@@ -76,36 +88,36 @@ for n in range(len(samples)):
             if combined.iat[i,0] != 0:
                 if combined.iat[i,1]-(combined.iat[i,1]/200000) < combined.iat[j,1] < combined.iat[i,1]+(combined.iat[i,1]/200000):
                     if abs(combined.iat[i,0] - combined.iat[j,0]) < .05:
-                        
                             combined.iat[i,0] = 0
     combined = combined.replace(0, np.nan)
     combined = combined.dropna(axis = 0)
     combined.sort_values(by=['13'], inplace=True)
     combos[samples.iat[n,0]] = combined
-    
+
+print(str(len(combined)) + "Likely 12C/13C pairs")    
     
 
 #%%load data
 # will need to convert data to .h5 format
 
-os.chdir("import the path name of the folder containing the .h5 files")
+os.chdir("/Users/tashiliebergesell/Desktop/20240814_AWP273_diff trace/0uM Cu, 5uM Fe") #Change path as needed
 data = {}
 data["atlas"] = 0
-data["lcmsrun"] = "XXX_P1.h5" #placeholder for the file location. Rename if needed. Make sure 13C + 12C-precursor samples are named, P1, P2, P3, etc. 
+data["lcmsrun"] = "AWP273_P1.h5" #placeholder for the file location. Rename if needed. Make sure 13C + 12C-precursor samples are named, P1, P2, P3, etc. 
 data["file_index"] = 1
 data["polarity"] = "positive"  #can switch polarity if needed
 ppm = 15 #change tolderance if needed
 
 data12 = {}
 data12["atlas"] = 0
-data12["lcmsrun"] = "XXX_12C.h5" #placeholder for the file location. Rename if needed. 
+data12["lcmsrun"] = "AWP273_12C.h5" #placeholder for the file location. Rename if needed. 
 data12["file_index"] = 2
 data12["polarity"] = "positive"  #can switch polarity if needed
 ppm2 = 15 #change tolderance if needed
 
 data13 = {}
 data13["atlas"] = 0
-data13["lcmsrun"] = "XXX_13C.h5" #placeholder for the file location. Rename if needed. 
+data13["lcmsrun"] = "AWP273_13C.h5" #placeholder for the file location. Rename if needed. 
 data13["file_index"] = 3
 data13["polarity"] = "positive"   #can switch polarity if needed
 ppm2 = 15 #change tolderance if needed
@@ -151,7 +163,7 @@ for n in range(len(samples)):
         if len(int13) == 1 and len(int12) == 1:
             bigger = max(int13.iat[0,3],int12.iat[0,3])
             smaller = min(int13.iat[0,3],int12.iat[0,3])
-            if bigger/smaller > 5:
+            if bigger/smaller > 5: #5 for qtof, 8 for orbi
                 z = 1
         else:
             z = 1
@@ -161,8 +173,12 @@ for n in range(len(samples)):
     combos[key] = combined
 print("--- %s seconds ---" % (time.time() - start_time))
 
-#%%neutron steps between boundary ions
+
+#%%
 import time
+import statistics
+import copy
+
 start_time = time.time()
 
 specs = {}
@@ -174,58 +190,77 @@ for n in range(len(samples)):
     choice[key] = {}
     data12['lcmsrun'] = key + '_12C.h5'
     data13['lcmsrun'] = key + '_13C.h5'
-    for o in [1,2,3]:   #can change number to however many 13C + 12C-precursor samples you have
+    for o in [1,2,3]:   # Can change number to however many 13C + 12C-precursor samples you have
         data['lcmsrun'] = key + '_P' + str(o) + ".h5"
         specs[key]['P' + str(o)] = {}
         choice[key]['P' + str(o)] = {}
 
-        print('getting isotopologue data for file {}:'.format(o))
+        print('Getting isotopologue data for file {}:'.format(o))
         for i in tqdm(range(len(combined))):
-            steps = math.ceil(combined.iat[i,1] - combined.iat[i,3])
+            steps = math.ceil(combined.iat[i, 1] - combined.iat[i, 3])
             subst = []
-            RT = (combined.iat[i,0] + combined.iat[i,2])/2
-            for j in range(-1,steps+1):
-                subst.append([combined.iat[i,3] + (j*1.003586),RT,RT+.1,RT-.1,ppm,j+2,0,j+3,j+1])
+            RT = (combined.iat[i, 0] + combined.iat[i, 2]) / 2
+            for j in range(-1, steps + 1):
+                subst.append([combined.iat[i, 3] + (j * 1.003586), RT, RT + .1, RT - .1, ppm, j + 2, 0, j + 3, j + 1])
             subst = pd.DataFrame(subst)
-            subst = subst.rename(columns={0:"mz",1:"rt_peak",2:"rt_max",3:"rt_min",4:"ppm_tolerance",
-                                          5:"group_index",6:"extra_time",7:"label",8:"index"})
+            subst = subst.rename(columns={0: "mz", 1: "rt_peak", 2: "rt_max", 3: "rt_min", 4: "ppm_tolerance",
+                                          5: "group_index", 6: "extra_time", 7: "label", 8: "index"})
             data12.update({"atlas": subst})
             data13.update({"atlas": subst})
             data.update({"atlas": subst})
 
             try:
-                d = ft.get_data(data,return_data=True,save_file=False)
-                d12 = ft.get_data(data12,return_data=True,save_file=False)
-                d13 = ft.get_data(data13,return_data=True,save_file=False)
+                d = ft.get_data(data, return_data=True, save_file=False)
+                d12 = ft.get_data(data12, return_data=True, save_file=False)
+                d13 = ft.get_data(data13, return_data=True, save_file=False)
             except:
-                # print('sample: {} o: {} i: {} no data returned'.format(n, o, i))
+                # print('Sample: {} o: {} i: {} no data returned'.format(n, o, i))
                 continue
 
             intensity = np.array(d["ms1_summary"]["peak_height"])
-            imax =  intensity.argmax()
-          
+            imax = intensity.argmax()
             d = d['ms1_summary']
             d12 = d12['ms1_summary']
             d13 = d13['ms1_summary']
-           
+            avgrt = statistics.mean(d['rt_peak'])
+
+            # ### New addition
+            label = list(range(2, max(data['atlas']['label'])))
+            d_fill = pd.DataFrame(label)
+            d_fill = d_fill.rename(columns={0: 'label'})
+            d_fill = d_fill.reindex(columns=d.columns)
+            for l in range(len(d_fill)):
+                hd = d.loc[d['label'] == d_fill.iat[l, 0], :]
+                if len(hd) > 0:
+                    hd = hd.reset_index(drop=True)
+                    d_fill.loc[l] = hd.loc[0]
+
+            d = copy.deepcopy(d_fill)
             d['12'] = 0
             d['13'] = 0
+            d['12'] = d['12'].astype(float)
+            d['13'] = d['13'].astype(float)
+
             for l in range(len(d)):
-                h12 = d12.loc[d12['label'] == d.iat[l,0],:]  
-                if len(h12) > 0:  
-                    d.iat[l,6] = h12.iat[0,3]  
+                h12 = d12.loc[d12['label'] == d.iat[l, 0], :]
+                if len(h12) > 0:
+                    d.loc[l, '12'] = float(h12.iat[0, 3])  
+
             for l in range(len(d)):
-                h13 = d13.loc[d13['label'] == d.iat[l,0],:]
+                h13 = d13.loc[d13['label'] == d.iat[l, 0], :]
                 if len(h13) > 0:
-                    d.iat[l,7] = h13.iat[0,3]
-            specs[key]['P' + str(o)][str([d.iat[0,5],d.iat[imax,3]])] = d[['label','mz_centroid','12','peak_height','13','rt_peak']]
+                    d.loc[l, '13'] = float(h13.iat[0, 3])  
+
+            d['12m/z'] = combined.iat[i, 3]
+            d['13m/z'] = combined.iat[i, 1]
+            specs[key]['P' + str(o)][str([d.iat[0, 5], d.iat[imax, 3]])] = d[['label', 'mz_centroid', '12', 'peak_height', '13', 'rt_peak', '12m/z', '13m/z']]
             worth = 0
-            for k in range(2,len(d)-2):
-                if d.iat[k,3] > d.iat[k,6]*2 and d.iat[k,3] > d.iat[k,7]*2:
+            for k in range(2, len(d) - 2):
+                if d.iat[k, 3] > d.iat[k, 6] * 2 and d.iat[k, 3] > d.iat[k, 7] * 2:
                     worth = 1
             if worth == 1:
-                choice[key]['P' + str(o)][str([d.iat[0,5],d.iat[imax,3]])] = d[['label','mz_centroid','12','peak_height','13','rt_peak']]
-            
+                choice[key]['P' + str(o)][str([avgrt, intensity[imax]])] = d[['label', 'mz_centroid', '12', 'peak_height', '13', 'rt_peak', '12m/z', '13m/z']]
+
 print("--- %s seconds ---" % (time.time() - start_time))
 
 
@@ -233,62 +268,68 @@ print("--- %s seconds ---" % (time.time() - start_time))
 
 import pickle
 
-with open ("/Users/guest/Desktop/XXX_choice.pkl", "wb") as file:  #change path name and file name as needed to save and export the choice.pkl file. 
+with open ("/Users/tashiliebergesell/Desktop/AWP273_choice.pkl", "wb") as file:
     pickle.dump(choice, file)
     file.close()
-    
+     
 
 
 #%%importing raw file for detected features
 
 import pickle
     
-with open ("/Users/guest/XXX_choice.pkl", "rb") as file:  #Import the choice.pkl file if needed
+with open ("/Users/tashiliebergesell/Desktop/20240814_AWP273_diff trace/0uM Cu, 5uM Fe/AWP273_choice.pkl", "rb") as file:
     loaded_dict = pickle.load(file)
     file.close()
 
 #%%Filtering out false positives
     
 import pandas as pd    
-AWP = loaded_dict['XXX']  #change name as needed. The key needs to be consistent across samples.  
+AWP = loaded_dict['AWP273']  # Change name as needed. Needs to be the same name chosen for the individual files. 
 items_to_delete = []  
 
-for key in ['P1', 'P2', 'P3']:  #change to however many 13C + 12C-precursor samples you have
+# Loop through all keys in the AWP dictionary (e.g., P1, P2, P3)
+for key in AWP.keys():
     for dataframe_key, dataframe in AWP[key].items(): 
+        dataframe['peak_height'] = dataframe['peak_height'].fillna(0)
         max_value12 = dataframe['12'].max()
         max_valuepeak = dataframe['peak_height'].max()
         max_value13 = dataframe['13'].max()
         
-        mzcent12mz = dataframe.loc[dataframe['12'] == max_value12, 'mz_centroid'].values[0]    
+        mzcent12mz = dataframe.loc[dataframe['12'] == max_value12, 'mz_centroid'].values[0] 
         mzcentpeakmz = dataframe.loc[dataframe['peak_height'] == max_valuepeak, 'mz_centroid'].values[0]
         mzcent13mz = dataframe.loc[dataframe['13'] == max_value13, 'mz_centroid'].values[0]
-        if mzcent12mz < mzcentpeakmz and mzcentpeakmz < mzcent13mz and mzcent12mz < mzcent13mz and (mzcent13mz - mzcent12mz) > 10: ##UPDATE THESE PARAMETERS OR ADD OTHERS IF NEEDED
+        
+        if mzcent12mz < mzcentpeakmz and mzcentpeakmz < mzcent13mz and mzcent12mz < mzcent13mz:
             pass
         else:
             items_to_delete.append((key, dataframe_key))
-  
-for key, dataframe_key in items_to_delete:
-    del loaded_dict['XXX'][key][dataframe_key]  #change name as needed. The key needs to be consistent across samples. 
 
-for key in ['P1', 'P2', 'P3']:  #change the number of samples as needed
-    for dataframe_key, dataframe in loaded_dict['XXX'][key].items():  #change name as needed. Needs to be the same name chosen for the individual files. 
+# Perform the deletion outside the loop
+for key, dataframe_key in items_to_delete:
+    del AWP[key][dataframe_key]  # Change name as needed. Needs to be the same name chosen for the individual files.
+
+for key in AWP.keys():
+    for dataframe_key, dataframe in AWP[key].items():
         dataframe.rename(columns={'12': '12C_intensity', '13': '13C_intensity', 'peak_height': '13C+12C_precursor_intensity'}, inplace=True)
+
+
+
 
 #%%
 # save reduced pkl file
-with open ("/Users/guest/XXX_reduced.pkl", "wb") as file:
+with open ("/Users/tashiliebergesell/Desktop/AWP273_reduced.pkl", "wb") as file:
      pickle.dump(loaded_dict, file)
      file.close()  
-
-
+     
 #%% pull out and compile data
 
-combined_data = pd.DataFrame(columns=['P', 'RT', 'Mz_Centroid_12C', 'Max_12C_intensity', 'Mz_Centroid_13C+12C_precursor', 'Max_13C+12C_precursor_intensity', 'Mz_Centroid_13C', 'Max_13C_intensity', 'Delta_mass'])
+data_list = []
 
-for key in ['P1', 'P2', 'P3']: #change to however many samples you have
-    for feature_key, feature_df in loaded_dict['XXX'][key].items():   #change name as needed. Key needs to be consistent with individual files. 
+for key in ['P1']:
+    for feature_key, feature_df in loaded_dict['AWP273'][key].items():  
        
-        max_12C_intensity = feature_df['12C_intensity'].max()  
+        max_12C_intensity = feature_df['12C_intensity'].max()  # Extracts the maximum value from the 12C_intensity column in the current feature_df dataframe
         max_13C_precursor_intensity = feature_df['13C+12C_precursor_intensity'].max()
         max_13C_intensity = feature_df['13C_intensity'].max()
        
@@ -297,8 +338,9 @@ for key in ['P1', 'P2', 'P3']: #change to however many samples you have
         mz_centroid_13C = feature_df.loc[feature_df['13C_intensity'] == max_13C_intensity, 'mz_centroid'].values[0]
         RT = feature_df.loc[feature_df['12C_intensity'] == max_12C_intensity, 'rt_peak'].values[0]
         delta_mass = mz_centroid_13C - mz_centroid_13C_precursor
-   
-        combined_data = combined_data.append({
+        
+        # Append data to the list
+        data_list.append({
             'P': key,
             'RT': RT,
             'Mz_Centroid_12C': mz_centroid_12C,
@@ -308,11 +350,11 @@ for key in ['P1', 'P2', 'P3']: #change to however many samples you have
             'Mz_Centroid_13C': mz_centroid_13C,
             'Max_13C_intensity': max_13C_intensity,
             'Delta_mass': delta_mass
-        }, ignore_index=True)
+        })
+
+# Convert the list to a DataFrame
+combined_data = pd.DataFrame(data_list)
 
 compiled_data = combined_data.sort_values(by=["P", "RT"])
 
-compiled_data.to_csv('path name.csv') #save as .csv. Change name and path as needed.
-    
-
-
+compiled_data.to_csv('/Users/tashiliebergesell/Desktop/compiled data AWP273.csv')
